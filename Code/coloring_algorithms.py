fa15351d88c7303709a04038a1e9c7a67e97df5a
nizwant <mateusznizwantowski@gmail.com>
2023-04-19 19:40:52 +0200

    Lots of repeated code but i want clear distinction between with interchange and without

--- Code/coloring_algorithms.py
+++ Code/coloring_algorithms.py
@@ -62,6 +62,16 @@ def random_sequential(G):
     return coloring, chromatic_number
 
 
+def random_sequential_with_interchange(G):
+    # obtain list of nodes in graph G and shuffle it
+    order = list(G.nodes())
+    random.shuffle(order)
+
+    # use greedy on it
+    coloring, chromatic_number = greedy(G, order, color_with_interchange=True)
+    return coloring, chromatic_number
+
+
 def largest_first(G):
     # obtain list of nodes in graph G and sort it
     order = G.nodes()
@@ -70,6 +80,17 @@ def largest_first(G):
 
     # use greedy on it
     coloring, chromatic_number = greedy(G, order)
+    return coloring, chromatic_number
+
+
+def largest_first_with_interchange(G):
+    # obtain list of nodes in graph G and sort it
+    order = G.nodes()
+    order = sorted(order, key=lambda x: G.degree(x), reverse=True)
+    print(order)
+
+    # use greedy on it
+    coloring, chromatic_number = greedy(G, order, color_with_interchange=True)
     return coloring, chromatic_number
 
 
@@ -112,8 +133,64 @@ def smallest_last(G):
     return coloring, chromatic_number
 
 
+def smallest_last_with_interchange(G):
+    degrees_vertices = defaultdict(set)
+    min_degree = float("inf")
+
+    # Iterate over all vertices in the graph in order to create degrees_vertices
+    # dictionary with key:value pairs degree:{nodes_that_have_this_degree}
+    for v in G:
+        min_degree = min(min_degree, G.degree(v))
+        degrees_vertices[G.degree(v)].add(v)
+
+    H = G.copy()
+    order = []
+
+    while degrees_vertices:  # Loop until all vertices have been removed
+        while min_degree not in degrees_vertices:
+            min_degree += 1
+
+        # Remove a vertex from set with the current minimum degree and delate set if empty
+        min_degree_vertex = degrees_vertices[min_degree].pop()
+        if len(degrees_vertices[min_degree]) == 0:
+            degrees_vertices.pop(min_degree)
+
+        # Update the degrees of the neighbors of the removed vertex (decrease degree by 1)
+        # Transfer vertex from set 'degree' to 'degree - 1' to reflect the removal of its neighbor
+        for neighbor in H.neighbors(min_degree_vertex):
+            degree = H.degree(neighbor)
+            degrees_vertices[degree].remove(neighbor)
+            if len(degrees_vertices[degree]) == 0:
+                degrees_vertices.pop(degree)
+            degrees_vertices[degree - 1].add(neighbor)
+
+        H.remove_node(min_degree_vertex)  # Remove the vertex from the graph
+        order.append(min_degree_vertex)  # Add the removed vertex to the order list
+        min_degree += -1
+
+    coloring, chromatic_number = greedy(
+        G, order[::-1], color_with_interchange=True
+    )  # greedy on reverse order
+    return coloring, chromatic_number
+
+
+def d_satur(G):
+    order = []
+    # TODO inplementacja ona tu i tu powinna byc identyczna
+    # wiem że mozna to zrobic ładniej ale chce funkcje z osobnymi nazwami a nie np jako argument
+    coloring, chromatic_number = greedy(G, order[::-1])
+    return coloring, chromatic_number
+
+
+def d_satur_with_interchange(G):
+    order = []
+    # TODO inplementacja
+    coloring, chromatic_number = greedy(G, order[::-1], color_with_interchange=True)
+    return coloring, chromatic_number
+
+
 def try_interchanging_colors(G, node, node_colors):
-    pass
+    return
 
 
 print(smallest_last(G))
